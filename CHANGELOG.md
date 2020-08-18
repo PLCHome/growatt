29b592f815a8276e115734bfcb4b138657529b4e
ChrisHanuta <GitHub@traeger-welt.de>
2020-08-18 23:21:50 +0200

    **Enhancements**
    - getAllPlantData and getAllPlantDeviceData: options added for selective api calls.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,6 @@
+### 0.0.2: Initial Release
+
+**Enhancements** 
+- getAllPlantData and getAllPlantDeviceData: options added for selective api calls.
+
 ### 0.0.0: Initial Release
--- test.js
+++ test.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 
 async function test() {
   let login = await Growatt.demoLogin().catch(e => {console.log(e)})
-  await Growatt.getPlatList().catch(e => {console.log(e)})
   let allPlantData = await Growatt.getAllPlantData().catch(e => {console.log(e)})
   console.log('Fatched all Data')
   fs.writeFileSync('test.json', JSON.stringify(allPlantData,null,' '));
