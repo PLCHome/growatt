ccc18437802f2fb2c00b19cd7395242e3edcc922
ChrisHanuta <GitHub@traeger-welt.de>
2024-07-03 18:04:44 +0200

    Job FIX

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,10 @@
-
 module.exports = {
-  files: ["**/*.js"],
-  /*env: {
+  env: {
     browser: true,
     es2017: true,
     node: true,
     mocha: true,
-  }, 
+  },
   extends: ['eslint:recommended', 'airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2018,
@@ -14,21 +12,10 @@ module.exports = {
       impliedStrict: true,
     },
     sourceType: 'module',
-  },*/
+  },
   rules: {
     'no-use-before-define': ['error', { functions: false }],
     'no-continue': 'off',
     'no-param-reassign': ['error', { props: false }],
   },
-  ignores: ["docs",
-    "test",
-    "lib/env/tools.js",
-    "gulpfile.js",
-    "test.json",
-    "test.js",
-    "test2.js",
-    "test_plants.json",
-    "testSharePlant.json"
-    ],
-   
 };
