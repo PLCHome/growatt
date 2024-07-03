66628967e44cda093658329247ddeb70e6bd4948
ChrisHanuta <GitHub@traeger-welt.de>
2024-07-03 14:43:05 +0200

    Change Logon

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
+
 module.exports = {
-  env: {
+  files: ["**/*.js"],
+  /*env: {
     browser: true,
     es2017: true,
     node: true,
     mocha: true,
-  },
+  }, 
   extends: ['eslint:recommended', 'airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2018,
@@ -12,10 +14,21 @@ module.exports = {
       impliedStrict: true,
     },
     sourceType: 'module',
-  },
+  },*/
   rules: {
     'no-use-before-define': ['error', { functions: false }],
     'no-continue': 'off',
     'no-param-reassign': ['error', { props: false }],
   },
+  ignores: ["docs",
+    "test",
+    "lib/env/tools.js",
+    "gulpfile.js",
+    "test.json",
+    "test.js",
+    "test2.js",
+    "test_plants.json",
+    "testSharePlant.json"
+    ],
+   
 };
